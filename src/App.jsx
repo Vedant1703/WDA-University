1d160fd1966b515dfa5d5fcbf81fcaeacbdd4499
Vedant <vedantkulkarni@vedants-MacBook-Air.local>
2025-02-10 00:16:32 +0530

    Dekh lo

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
-import Courses from './pages/Courses';
-import Contact from './pages/Contact';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -14,8 +13,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/courses" element={<Courses />} />
-          <Route path="/contact" element={<Contact />} />
+        
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </Router>
